970c6ad081c1f6d12b34d36af2b789da43537f82
2472076 <2472076@LTIN461372>
2026-04-20 16:15:16 +0530

    Pulling

--- src/pages/Supervisor/Cases/Cases.jsx
+++ src/pages/Supervisor/Cases/Cases.jsx
@@ -34,21 +34,14 @@ const Cases = () => {
     const caseRes = await axios.get("https://localhost:7027/api/Case/all");
     const appRes = await getApplications();
     const deptRes = await axios.get("https://localhost:7027/api/Department/active");
-
+ 
     // NEW: fetch services
     const serviceRes = await axios.get("https://localhost:7027/api/Services");
-
+ 
+ 
+   
+ 
     setCases(caseRes.data);
-  const slaRes = await axios.get("https://localhost:7027/api/Case/sla-breached");
-  const breachedCaseIds = new Set(
-  (slaRes.data || []).map(s => s.caseId)
-  );
-  const updatedCases = caseRes.data.map(c => ({
-  ...c,
-  isEscalated: c.isEscalated || breachedCaseIds.has(c.caseId)
-  }));
-
-    setCases(updatedCases);
     setDepartments(deptRes.data);
     setServices(serviceRes.data);
  
@@ -238,8 +231,8 @@ const Cases = () => {
                 <td>{c.serviceName}</td>
                 <td>{c.departmentName}</td>
                 <td>{c.officerName}</td>
-                <td>{c.isEscalated ? "Escalated" : c.status}</td>
-               <td>
+                <td>{c.status}</td>
+                <td>
                   <button
                     className="outline-btn"
                     onClick={() => navigate(`../case-details/${c.caseId}`)}
